a974575513a469a2ba750a9aa063d3f84b71f3b0
Carsten Dietzel <carsten.dietzel@check24.de>
2023-05-02 19:28:13 +0200

    Consider user to be undefined

--- src/access/checkRole.ts
+++ src/access/checkRole.ts
@@ -1,13 +1,6 @@
 import type { User } from '../payload-types';
 
-export const checkRole = (user: User, allRoles: User['roles'] = []): boolean => {
-    if (
-        allRoles.some(role => {
-            return user.roles.includes(role);
-        })
-    ) {
-        return true;
-    }
+export const checkRole = (user?: User, allRoles: User['roles'] = []): boolean => {
 
-    return false;
+    return user !== undefined && allRoles.some(role => user.roles.includes(role));
 };
