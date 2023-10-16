d2b404cb8c9b1db9248eed14c21b16d09030d0d2
Kennet <kennet@seebruecke.org>
2023-10-16 21:31:15 +0200

    Update Environment Files

--- src/types/environment.d.ts
+++ src/types/environment.d.ts
@@ -1,6 +1,14 @@
 declare namespace NodeJS {
     export interface ProcessEnv {
+        TZ: string;
+
         PAYLOAD_SECRET: string;
         MONGODB_URI: string;
+
+        COOKIE_DOMAIN: string;
+        PAYLOAD_PUBLIC_CMS_URL: string;
+        PAYLOAD_PUBLIC_SITE_URL: string;
+
+        REVALIDATION_KEY: string;
     }
 }
