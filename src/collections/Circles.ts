51731fb97cd3e05a6ec97df50a8172a16b3a32c6
Carsten Dietzel <carsten.dietzel@check24.de>
2024-08-21 21:17:55 +0200

    Try to clarify meaning of fallback image

--- src/collections/Circles.ts
+++ src/collections/Circles.ts
@@ -100,9 +100,12 @@ const Circles: CollectionConfig = {
                         },
                         {
                             name: 'fallbackImage',
-                            label: 'Fallback-Image',
+                            label: 'Strichmännchen-Bild',
                             type: 'text',
                             required: true,
+                            admin: {
+                                description: 'Name eines auf dem Server liegenden Strichmännchen-Bildes',
+                            },
                         },
                     ],
                 },
