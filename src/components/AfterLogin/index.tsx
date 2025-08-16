63489ced9edc11e0c1308ff4c382efec7d5b673c
Carsten Dietzel <carsten.dietzel@check24.de>
2025-08-16 13:29:57 +0200

    Merge BeforeLogin into AdminLogin component

--- src/components/AfterLogin/index.tsx
+++ src/components/AfterLogin/index.tsx
@@ -6,8 +6,23 @@ import Link from 'next/link';
 
 export const AdminLogin = () => {
     return (
-        <Link href="/api/users/oauth/authorize">
-            <Button>Einloggen</Button>
-        </Link>
+        <div>
+            <div>
+                <h4 style={{ fontSize: '20px', lineHeight: '1', marginBottom: 'calc(var(--base) * 1.2)' }}>
+                    <center>Logge dich hier mit deinem B-Side Account ein!</center>
+                </h4>
+
+                <p>
+                    Falls dir beim Login eine Fehlermeldung angezeigt wird, fehlen dir die notwendigen Rechte für den Zugriff auf das
+                    Backend unserer Webseite. Wende dich in dem Fall bitte bei Mattermost an das IT-Team.
+                </p>
+
+                <center style={{ transform: 'scale(1.2)' }}>
+                    <Link href="/api/users/oauth/authorize">
+                        <Button>Einloggen</Button>
+                    </Link>
+                </center>
+            </div>
+        </div>
     );
 };
